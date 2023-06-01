ea7d2f9087df9f9188fde860aafdf00569ebbf8a
sheldyrivaldi <sheldyrivaldi@gmal.com>
2023-06-02 05:15:54 +0700

    Day 10

--- js/contact.js
+++ js/contact.js
@@ -1,31 +1,40 @@
-function submitData(){
-    //Deklarasi variabel
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone-number").value;
-    let subject = document.getElementById("subject").value;
-    let message = document.getElementById("message").value;
+let subjectItem = "";
+let dropdownItems = ["Frontend Developer", "Backend Developer", "Fullstack Developer"];
 
-    //Validasi input data
-    if(name == ""){
-        return alert("Nama tidak boleh kosong!");
-    }
-    if(email == ""){
-        return alert("Email tidak boleh kosong!");
-    }
-    if(phone == ""){
-        return alert("Nomor HP tidak boleh kosong!");
-    }
-    if(subject == ""){
-        return alert("Subject tidak boleh kosong!");
-    }
+function onFire(item) {
+  subjectItem = dropdownItems[item];
+}
 
-    //Link "mailto"
-    let recivedEmail = "sheldyrivaldi@gmail.com";
-    let enter = "\n\n"
-    let a = document.createElement("a");
-    let mailToLink = `mailto:${recivedEmail}?subject=${encodeURIComponent(subject)}&body=Halo! Nama saya ${encodeURIComponent(name)}.${encodeURIComponent(enter)}${encodeURIComponent(message)}.${encodeURIComponent(enter)} Kamu bisa mengubungi saya pada nomor ${encodeURIComponent(phone)}.${encodeURIComponent(enter)} Terimakasih.`;
-    a.href = mailToLink;
+function submitData() {
+  //Deklarasi variabel
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let subject = subjectItem;
+  let message = document.getElementById("message").value;
 
-    a.click();
+  //Validasi input data
+  if (name == "") {
+    return alert("Nama tidak boleh kosong!");
+  }
+  if (email == "") {
+    return alert("Email tidak boleh kosong!");
+  }
+  if (phone == "") {
+    return alert("Nomor HP tidak boleh kosong!");
+  }
+  if (subject == "") {
+    return alert("Subject tidak boleh kosong!");
+  }
+
+  //Link "mailto"
+  let recivedEmail = "sheldyrivaldi@gmail.com";
+  let enter = "\n\n";
+  let a = document.createElement("a");
+  let mailToLink = `mailto:${recivedEmail}?subject=${encodeURIComponent(subject)}&body=Halo! Nama saya ${encodeURIComponent(name)}.${encodeURIComponent(enter)}${encodeURIComponent(message)}.${encodeURIComponent(
+    enter
+  )} Kamu bisa mengubungi saya pada nomor ${encodeURIComponent(phone)}.${encodeURIComponent(enter)} Terimakasih.`;
+  a.href = mailToLink;
+
+  a.click();
 }
