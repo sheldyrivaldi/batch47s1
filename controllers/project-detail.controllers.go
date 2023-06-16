1802cbd423b9c4bb2bcc61d3d54d1b4157191eb6
sheldyrivaldi <sheldyrivaldi@gmal.com>
2023-06-16 11:24:39 +0700

    Day 16 Update

--- controllers/project-detail.controllers.go
+++ controllers/project-detail.controllers.go
@@ -2,9 +2,12 @@ package controllers
 
 import (
 	"net/http"
+	"stage1/models"
 	"stage1/utilities"
 	"strconv"
 	"text/template"
+
+	"github.com/labstack/echo-contrib/session"
 
 	"github.com/labstack/echo/v4"
 )
@@ -22,8 +25,20 @@ func GetProjectDetailController(c echo.Context) error {
 	data.StartDate = data.StartDateTime.Format("01 Jan 2006")
 	data.EndDate = data.EndDateTime.Format("01 Jan 2006")
 
+	//Define session value to session model
+	sess, _ := session.Get("session", c)
+	var sessionData models.SessionData
+
+	if sess.Values["isLoggedIn"] != true {
+		sessionData.IsLoggedIn = false
+	} else {
+		sessionData.IsLoggedIn = sess.Values["isLoggedIn"].(bool)
+		sessionData.Username = sess.Values["username"].(string)
+	}
+
 	projects := map[string]interface{}{
-		"Projects": data,
+		"Projects":    data,
+		"SessionData": sessionData,
 	}
 
 	tmpl, err := template.ParseFiles("views/project-detail.html")
